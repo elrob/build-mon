75d06faf0c48cf3b29b276e4b83d739b527d4196
Rob Speller <rspeller@thoughtworks.com>
2015-12-13 21:02:02 +0000

    Refactor tests so expected result comes before actual result; use tabular 'are' function for refresh rate tests

--- test/build_mon/test/core.clj
+++ test/build_mon/test/core.clj
@@ -2,37 +2,38 @@
   (:require [clojure.test :refer :all]
             [build-mon.core :as c]))
 
-(def succeeded-build {:result "succeeded"})
+(def succeeded-build   {:result "succeeded"})
 (def in-progress-build {:result nil :status "inProgress"})
-(def failed-build {:result "failed"})
+(def failed-build      {:result "failed"})
 
 (deftest determine-background-colour
-  (is (= (c/determine-background-colour succeeded-build :anything) "green"))
-  (is (= (c/determine-background-colour failed-build :anything) "red"))
-  (is (= (c/determine-background-colour in-progress-build succeeded-build) "yellow"))
-  (is (= (c/determine-background-colour in-progress-build failed-build) "orange")))
+  (is (=  "green"  (c/determine-background-colour succeeded-build :anything)))
+  (is (=  "red"    (c/determine-background-colour failed-build :anything)))
+  (is (=  "yellow" (c/determine-background-colour in-progress-build succeeded-build)))
+  (is (=  "orange" (c/determine-background-colour in-progress-build failed-build))))
 
 (deftest determine-status-text
-  (is (= (c/determine-status-text succeeded-build) "succeeded"))
-  (is (= (c/determine-status-text failed-build) "failed"))
-  (is (= (c/determine-status-text in-progress-build) "inProgress")))
+  (is (= "succeeded"  (c/determine-status-text succeeded-build)))
+  (is (= "failed"     (c/determine-status-text failed-build)))
+  (is (= "inProgress" (c/determine-status-text in-progress-build))))
 
 (deftest determine-build-author
-  (is (= (c/determine-build-author {:requestedFor {:displayName "Bob"}}) "Bob"))
-  (is (= (c/determine-build-author {}) "N/A")))
+  (is (= "Bob" (c/determine-build-author {:requestedFor {:displayName "Bob"}})))
+  (is (= "N/A" (c/determine-build-author {}))))
 
 (deftest determine-text
   (let [build {:buildNumber "123"
                :result "succeeded"
                :requestedFor {:displayName "Bob"}}]
-  (is (= (c/determine-text build) "123 – Bob – succeeded"))))
+  (is (= "123 – Bob – succeeded" (c/determine-text build)))))
 
 (deftest determine-refresh-rate
   (let [default-rate 20]
-    (is (= (c/determine-refresh-rate {}) default-rate))
-    (is (= (c/determine-refresh-rate {"refresh" "true"}) default-rate))
-    (is (= (c/determine-refresh-rate {"refresh" "yes"}) default-rate))
-    (is (= (c/determine-refresh-rate {"refresh" "false"}) nil))
-    (is (= (c/determine-refresh-rate {"refresh" "no"}) nil))
-    (is (= (c/determine-refresh-rate {"refresh" "30"}) 30))
-    (is (= (c/determine-refresh-rate {"refresh" "rubbish"}) nil))))
+    (are [params               expected] (= expected (c/determine-refresh-rate params))
+         {}                    default-rate
+         {"refresh" "true"}    default-rate
+         {"refresh" "yes"}     default-rate
+         {"refresh" "30"}      30
+         {"refresh" "false"}   nil
+         {"refresh" "no"}      nil
+         {"refresh" "rubbish"} nil)))
