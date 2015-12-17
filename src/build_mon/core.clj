1efbf1f8feed3d43ee1ba4089833392009c18c90
Rob Speller <rspeller@thoughtworks.com>
2015-12-17 21:21:48 +0000

    Include commit message; remove build author

--- src/build_mon/core.clj
+++ src/build_mon/core.clj
@@ -24,18 +24,6 @@
 (defn determine-status-text [build]
   (if (in-progress? build) (:status build) (:result build)))
 
-(defn determine-build-author [build]
-  (or (-> build :requestedFor :displayName) "N/A"))
-
-(defn determine-text [build]
-  (let [sep " – "]
-    (str
-      (:buildNumber build)
-      sep
-      (determine-build-author build)
-      sep
-      (determine-status-text build))))
-
 (defn tryparse [string]
   (try (Integer. string)
        (catch Exception e nil)))
@@ -47,36 +35,53 @@
       ("false" "no") nil
       (tryparse refresh))))
 
-(defn generate-html [build previous-build refresh]
+(defn generate-html [build previous-build commit-message refresh]
   (let [background-colour (determine-background-colour build previous-build)
         favicon-filename (background-colour-key->favicon-filename background-colour)
         font-colour (if (in-progress? build) "black" "white")
-        text (determine-text build)]
+        status-text (determine-status-text build)]
     (str "<head>"
          "<title>Build Status</title>"
          (when refresh (str "<meta http-equiv=\"refresh\" content=\"" refresh "\" />"))
          "<link rel=\"shortcut icon\" href=\"" favicon-filename "\" />"
          "</head>"
          "<body style=\"background-color:" (background-colour background-colours) ";\">"
-         "<h1 style=\"color:" font-colour ";font-size:400%;text-align:center;\">" text "</h1>"
+         "<h1 style=\"color:" font-colour ";font-size:400%;text-align:center;\">" status-text "</h1>"
+         "<h1 style=\"color:" font-colour ";font-size:300%;text-align:center;\">" (:buildNumber build) "</h1>"
+         "<div style=\"color:" font-colour ";font-size:300%;text-align:center;\">" commit-message "</div>"
          "</body>")))
 
-(defn handler [account project token request]
+(defn get-commit-message [account token build]
+  (let [repository-id (-> build :repository :id)
+        source-version (:sourceVersion build)
+        commit-url (str "https://" account ".visualstudio.com/defaultcollection/_apis/git/repositories/"
+                        repository-id "/commits/" source-version "?api-version=1.0")]
+    (try (-> (client/get commit-url {:basic-auth ["USERNAME CAN BE ANY VALUE" token]})
+             :body (json/parse-string true) :comment)
+         (catch Exception e))))
+
+(defn get-last-two-builds [account project token]
   (let [last-two-builds-url (str "https://" account  ".visualstudio.com/defaultcollection/"
-                                 project "/_apis/build/builds?api-version=2.0&$top=2")
-        api-response (client/get last-two-builds-url {:basic-auth ["USERNAME CAN BE ANY VALUE" token]})
-        [build previous-build] (try (-> api-response :body (json/parse-string true) :value)
-                                    (catch Exception e))
+                                 project "/_apis/build/builds?api-version=2.0&$top=2")]
+    (try (-> (client/get last-two-builds-url {:basic-auth ["USERNAME CAN BE ANY VALUE" token]})
+             :body (json/parse-string true) :value)
+         (catch Exception e))))
+
+(defn handler [account project token request]
+  (let [[build previous-build] (get-last-two-builds account project token)
+        commit-message (get-commit-message account token build)
         refresh (determine-refresh-rate (:query-params request))]
     (prn "--------------------------------------")
+    (prn (str "Commit message: " commit-message))
     (prn (str "Build - Result: " (:result build)))
     (prn (str "Build - Status: " (:status build)))
     (prn (str "Prev  - Result: " (:result build)))
     (prn (str "Prev  - Status: " (:status build)))
+    (prn "--------------------------------------")
     (when build
       {:status 200
        :headers {"Content-Type" "text/html; charset=utf-8"}
-       :body (generate-html build previous-build refresh)})))
+       :body (generate-html build previous-build commit-message refresh)})))
 
 (defn -main [& [vso-account vso-project vso-personal-access-token port]]
   (let [port (Integer. (or port 3000))]
--- test/build_mon/test/core.clj
+++ test/build_mon/test/core.clj
@@ -18,16 +18,6 @@
        (c/determine-status-text failed-build) => "failed"
        (c/determine-status-text in-progress-build) => "inProgress")
 
-(facts "determine-build-author"
-       (c/determine-build-author {:requestedFor {:displayName "Bob"}}) => "Bob"
-       (c/determine-build-author {}) => "N/A")
-
-(facts "determine-text"
-       (let [build {:buildNumber "123"
-                    :result "succeeded"
-                    :requestedFor {:displayName "Bob"}}]
-         (c/determine-text build) => "123 – Bob – succeeded"))
-
 (let [default-rate 20]
   (tabular
     (facts "determine-refresh-rate"
@@ -46,7 +36,14 @@
         filenames-in-public-directory => (contains required-favicon-filenames :in-any-order :gaps-ok)))
 
 (facts "about generating html"
-       (let [successful-build-html (c/generate-html succeeded-build succeeded-build 20)]
+       (let [build {:result "succeeded" :buildNumber "2015.12.17.04"}
+             successful-build-html (c/generate-html build succeeded-build "great commit" 20)]
+         (fact "build status is displayed"
+               successful-build-html => (contains "succeeded"))
+         (fact "build number is displayed"
+               successful-build-html => (contains "2015.12.17.04"))
+         (fact "commit message is displayed"
+               successful-build-html => (contains "great commit"))
          (fact "favicon filename is included in link tag"
                successful-build-html => (contains "<link rel=\"shortcut icon\" href=\"favicon_green.ico\" />"))
          (fact "body has a green background"
@@ -55,7 +52,7 @@
                successful-build-html => (contains "<h1 style=\"color:white;font-size:400%;text-align:center;\">")))
 
        (fact "refresh line is generated when refresh value is passed"
-             (c/generate-html succeeded-build succeeded-build 20) => (contains "<meta http-equiv=\"refresh\" content=\"20\" />")
-             (c/generate-html succeeded-build succeeded-build 60) => (contains "<meta http-equiv=\"refresh\" content=\"60\" />"))
+             (c/generate-html succeeded-build succeeded-build "commit" 20) => (contains "<meta http-equiv=\"refresh\" content=\"20\" />")
+             (c/generate-html succeeded-build succeeded-build "commit" 60) => (contains "<meta http-equiv=\"refresh\" content=\"60\" />"))
        (fact "refresh line is not generated if nil refresh value is passed"
-             (c/generate-html succeeded-build succeeded-build nil) =not=> (contains "meta")))
+             (c/generate-html succeeded-build succeeded-build "commit "nil) =not=> (contains "meta")))
