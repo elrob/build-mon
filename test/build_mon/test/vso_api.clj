4c51adf4657079e2ab857a40d094278fae2fd3e2
Rob Speller <rspeller@thoughtworks.com>
2016-02-27 23:25:39 +0000

    Test retrieve-build-info function

--- test/build_mon/test/vso_api.clj
+++ test/build_mon/test/vso_api.clj
@@ -5,10 +5,9 @@
 
 (def account "VSO_ACCOUNT_NAME")
 (def project "VSO_PROJECT_NAME")
-(def token "VSO_API_TOKEN")
 
 (fact "vso-api-get-fn wraps an api token and returns a function which makes a HTTP GET request on a url"
-      (let [result (api/vso-api-get-fn token)]
+      (let [result (api/vso-api-get-fn "VSO_API_TOKEN")]
         (fn? result) => truthy
         (result "NOT A REAL URL") => (throws java.net.MalformedURLException)))
 
@@ -18,30 +17,57 @@
         (fn? (:retrieve-build-info result)) => truthy
         (fn? (:retrieve-build-definitions result)) => truthy))
 
-(defn get-fn-stub-request [stubbed-url response]
+(defn get-fn-stub-requests [stubbed-url->stubbed-response-map]
   (fn [url]
-    (when (= stubbed-url url) response)))
+    (get stubbed-url->stubbed-response-map url
+         {:URL :MISMATCH :url url :stubbed-urls (keys stubbed-url->stubbed-response-map)})))
 
 (facts "retrieve-build-definitions"
-       (let [expected-url "https://VSO_ACCOUNT_NAME.visualstudio.com/defaultcollection/VSO_PROJECT_NAME/_apis/build/definitions?api-version=2.0"]
+       (let [expected-url (str "https://" account ".visualstudio.com/defaultcollection/" project
+                               "/_apis/build/definitions?api-version=2.0")]
          (fact "calls VSO api, parses response and extracts build definitions"
-               (let [stub-json-body "{\"count\":2,\"value\":[{\"A_BUILD_DEFINITION_KEY\": \"A_VALUE\"},
+               (let [stub-json-body "{\"count\":2,\"value\":[
+                                    {\"A_BUILD_DEFINITION_KEY\": \"A_VALUE\"},
                                     {\"ANOTHER_BUILD_DEFINITION_KEY\": \"ANOTHER_VALUE\"}]}"
                      stub-response {:status 200 :body stub-json-body}
-                     get-fn-stubbed (get-fn-stub-request expected-url stub-response)
+                     get-fn-stubbed (get-fn-stub-requests {expected-url stub-response})
                      vso-api (api/vso-api-fns get-fn-stubbed account project)]
                  ((:retrieve-build-definitions vso-api)) => [{:A_BUILD_DEFINITION_KEY "A_VALUE"}
                                                              {:ANOTHER_BUILD_DEFINITION_KEY "ANOTHER_VALUE"}]))
          (fact "when response status is not 200, returns nil"
                (let [stub-response {:status 503}
-                     get-fn-stubbed (get-fn-stub-request expected-url stub-response)
+                     get-fn-stubbed (get-fn-stub-requests {expected-url stub-response})
                      vso-api (api/vso-api-fns get-fn-stubbed account project)]
 
                  ((:retrieve-build-definitions vso-api)) => nil))
          (fact "when response body cannot be parsed as JSON, returns nil"
                (let [stub-response {:status 200 :body "~~INVALID_JSON~~"}
-                     get-fn-stubbed (get-fn-stub-request expected-url stub-response)
+                     get-fn-stubbed (get-fn-stub-requests {expected-url stub-response})
                      vso-api (api/vso-api-fns get-fn-stubbed account project)]
                  ((:retrieve-build-definitions vso-api)) => nil))))
 
-
+(facts "retrieve-build-info"
+       (let [build-definition-id 666
+             repository-id "ABC-123"
+             source-version "XYZ-456"
+             expected-builds-url (str "https://" account ".visualstudio.com/defaultcollection/" project
+                                      "/_apis/build/builds?api-version=2.0&$top=2&definitions="
+                                      build-definition-id)
+             expected-commit-url (str "https://" account
+                                      ".visualstudio.com/defaultcollection/_apis/git/repositories/"
+                                      repository-id "/commits/" source-version "?api-version=1.0")]
+         (fact "calls VSO api for builds and commit message, parses responses and extracts build info"
+               (let [stub-builds-body (str "{\"count\":2,\"value\":["
+                                           "{\"repository\": {\"id\": \"" repository-id "\"},"
+                                           "\"sourceVersion\": \"" source-version "\"},"
+                                           "{\"some_previous_build_key\": \"some_value\"}]}")
+                     stub-builds-response {:status 200 :body stub-builds-body}
+                     stub-commit-response {:status 200 :body "{\"comment\": \"SOME COMMIT MESSAGE\"}"}
+                     get-fn-stubbed (get-fn-stub-requests {expected-builds-url stub-builds-response
+                                                           expected-commit-url stub-commit-response})
+                     vso-api (api/vso-api-fns get-fn-stubbed account project)]
+                 ((:retrieve-build-info vso-api) build-definition-id)
+                 => {:build {:repository {:id repository-id}
+                             :sourceVersion source-version}
+                     :previous-build {:some_previous_build_key "some_value"}
+                     :commit-message "SOME COMMIT MESSAGE"}))))
