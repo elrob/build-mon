2674ad2bdf5922b63b74d004a388a840024da000
Rob Speller <rspeller@thoughtworks.com>
2016-02-27 21:51:22 +0000

    Test retrieve-build-definitions function error handling

--- test/build_mon/test/vso_api.clj
+++ test/build_mon/test/vso_api.clj
@@ -22,12 +22,26 @@
   (fn [url]
     (when (= stubbed-url url) response)))
 
-(fact "retrieve-build-definitions retrieves build definitions"
-      (let [expected-url "https://VSO_ACCOUNT_NAME.visualstudio.com/defaultcollection/VSO_PROJECT_NAME/_apis/build/definitions?api-version=2.0"
-            stub-json-body "{\"count\":2,\"value\":[{\"A_BUILD_DEFINITION_KEY\": \"A_VALUE\"},
-                                                    {\"ANOTHER_BUILD_DEFINITION_KEY\": \"ANOTHER_VALUE\"}]}"
-            stub-response {:status 200 :body stub-json-body}
-            get-fn-stubbed (get-fn-stub-request expected-url stub-response)
-            vso-api (api/vso-api-fns get-fn-stubbed account project)]
-        ((:retrieve-build-definitions vso-api)) => [{:A_BUILD_DEFINITION_KEY "A_VALUE"}
-                                                    {:ANOTHER_BUILD_DEFINITION_KEY "ANOTHER_VALUE"}]))
+(facts "retrieve-build-definitions"
+       (let [expected-url "https://VSO_ACCOUNT_NAME.visualstudio.com/defaultcollection/VSO_PROJECT_NAME/_apis/build/definitions?api-version=2.0"]
+         (fact "calls VSO api, parses response and extracts build definitions"
+               (let [stub-json-body "{\"count\":2,\"value\":[{\"A_BUILD_DEFINITION_KEY\": \"A_VALUE\"},
+                                    {\"ANOTHER_BUILD_DEFINITION_KEY\": \"ANOTHER_VALUE\"}]}"
+                     stub-response {:status 200 :body stub-json-body}
+                     get-fn-stubbed (get-fn-stub-request expected-url stub-response)
+                     vso-api (api/vso-api-fns get-fn-stubbed account project)]
+                 ((:retrieve-build-definitions vso-api)) => [{:A_BUILD_DEFINITION_KEY "A_VALUE"}
+                                                             {:ANOTHER_BUILD_DEFINITION_KEY "ANOTHER_VALUE"}]))
+         (fact "when response status is not 200, returns nil"
+               (let [stub-response {:status 503}
+                     get-fn-stubbed (get-fn-stub-request expected-url stub-response)
+                     vso-api (api/vso-api-fns get-fn-stubbed account project)]
+
+                 ((:retrieve-build-definitions vso-api)) => nil))
+         (fact "when response body cannot be parsed as JSON, returns nil"
+               (let [stub-response {:status 200 :body "~~INVALID_JSON~~"}
+                     get-fn-stubbed (get-fn-stub-request expected-url stub-response)
+                     vso-api (api/vso-api-fns get-fn-stubbed account project)]
+                 ((:retrieve-build-definitions vso-api)) => nil))))
+
+
