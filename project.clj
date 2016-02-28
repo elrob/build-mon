299bd1abd38ab48c0cbf634cab1f8fc1230ec8cc
Rob Speller <rspeller@thoughtworks.com>
2016-02-28 01:10:28 +0000

    Add min-lein-version to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "localhost:3000"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [bidi "1.24.0"]
