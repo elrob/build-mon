c172b10d0e9fd8ce184985229b380ae56ffee1a6
Rufus Raghunath <rraghun@thoughtworks.com>
2016-10-02 18:33:40 +0100

    RR - fix forgotten bracket and remove dbugging prntln

--- src/build_mon/core.clj
+++ src/build_mon/core.clj
@@ -111,7 +111,7 @@
 
 (defn universal-monitor-for-definition-ids [vso-api vso-release-api request build-definition-ids release-definition-ids]
   (let [build-info-maps (remove nil? (map #(retrieve-build-info vso-api %) build-definition-ids))
-        release-info-maps (remove nil? (map #(retrieve-release-info vso-release-api %) release-definition-ids))
+        release-info-maps (remove nil? (map #(retrieve-release-info vso-release-api %) release-definition-ids))]
     (when (and (not-empty build-info-maps) (not-empty release-info-maps))
       (let [favicon-path (get-favicon-path build-info-maps release-info-maps)]
         {:status 200
